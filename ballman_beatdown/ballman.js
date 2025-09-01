f841f9f9ba5db797fd2966be677fc8d7cfd4b0c0
FireLordDX <ytfirekoopa@gmail.com>
2025-09-01 12:13:52 -0500

    fix url stuff

---
+++ ballman_beatdown/ballman.js
@@ -0,0 +1,40 @@
+//For the header buttons
+var curPage = '';
+            
+function showPage(daPage) {
+    if (curPage == daPage)
+        return;
+    
+    const pages = document.querySelectorAll('.page');
+    curPage = daPage;
+
+    pages.forEach(page => {
+        page.style.display = 'none';
+
+        page.querySelectorAll('.trans-in').forEach(e => {
+            e.classList.remove('visible');
+        });
+    });
+
+    document.getElementById(daPage).style.display = 'block';
+}
+
+window.onload = () => showPage('overview');
+
+//fancy shit as you scroll down and load more elements
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.trans-in');
+    
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) 
+            {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, {
+    threshold: 0.1
+    });
+
+    elements.forEach(e => observer.observe(e));
+});
